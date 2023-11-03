325ee638bd08e4fe70b554a4f0ac00ee223b90d4
Yasin Arafat <142558156+yasin-arafat-05@users.noreply.github.com>
2023-11-03 12:12:47 +0600

    Delete CodeWithHarrayProblem directory

--- CodeWithHarrayProblem/ChapterTWO/problem1.py
+++
@@ -1,3 +0,0 @@
-x = int(input("Enter first number: "))
-y = int(input("Enter second number: "))
-print(f"SUM: {x+y}")
--- CodeWithHarrayProblem/ChapterTWO/problem4.py
+++
@@ -1,6 +0,0 @@
-a = int(input("Enter first number : "))
-b = int(input("Enter second number : "))
-if(a>b):
-    print(a)
-else:
-    print(b)
