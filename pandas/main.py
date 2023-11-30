031ecf3e44e25e87720e42663abb77b6f62300bb
yasin-arafat-05 <yasinarafat.e2021@gmail.com>
2023-11-30 20:51:27 +0600

    pandas

--- pandas/main.py
+++ pandas/main.py
@@ -1,12 +1,16 @@
+import matplotlib
+matplotlib.use('TkAgg')
+
 # importing the required module 
 import matplotlib.pyplot as plt 
-
-x = [1, 2, 3] 
-y = [2, 4, 1] 
 	
+# x axis values 
+x = [1,2,3] 
+# corresponding y axis values 
+y = [2,4,1] 
 plt.plot(x, y) 
 plt.xlabel('x - axis') 
 plt.ylabel('y - axis') 
 plt.title('My first graph!') 
-
-plt.show(block=True)
+	
+plt.show() 
